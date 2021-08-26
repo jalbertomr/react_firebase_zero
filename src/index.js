3ec27887819ee165efef18af1476ba0cc956f655
jalbertomr <jalbertomr@yahoo.com>
2021-08-26 17:02:23 -0500

    use JSX, variables in JSX, conditionals

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const text = " 'text inserted'";
+const textColor = 'green';
+const inSession = true;
+
+const JSXcode = 
+  <div>
+    <h3 className="title" style={{color: textColor, backgroundColor: 'lightblue'}}>
+      React JSX inserting:{text}</h3>
+    <p>logged Ok. </p>
+  </div>;
+
+const isInSession = (inSession) => {
+  if(inSession === true){
+    return JSXcode;
+  } else {
+    return <h1>Session not started!</h1>
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <h1>React with very simple h1</h1>
-  </React.StrictMode>,
+  isInSession(inSession),
   document.getElementById('root')
 );
